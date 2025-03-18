d8cf7b0b9be1aece6b2ed6add0e9444ba9a6b170
WWetuig <klinkovicvadim7@gmail.com>
2025-03-18 21:50:56 +0700

    chore: updated requirements.txt. remover extra packages from main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,7 @@
-from fastapi import FastAPI, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
-from starlette import status
+from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from api.v1 import users, files
-from schemas.schemas import UserOut, UserCreate
-from core.security import verify_token, oauth2_scheme, verify_password, create_access_token
-from db.CRUD import get_link, get_user_by_login, create_user
-from db.database import get_db
-
-
 
 # Создание FastAPI приложения
 app = FastAPI()
