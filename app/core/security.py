3a239e764b3c3d1956d53d7c3605fd09bf44722d
WWetuig <klinkovicvadim7@gmail.com>
2025-03-18 16:49:54 +0700

    feat: added create_access_token and verify_token functions for user authorization

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,19 @@
+from datetime import timedelta, datetime, timezone
+from http.client import HTTPException
+from typing import Optional
+
+import jwt
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
+
+from core.config import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY
 
 # Инициализация контекста для хеширования паролей
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+#объект oauth2_scheme для реализации механизма аутентификации с использованием OAuth 2.0
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 #хеширование пароля
 def hash_password(password: str) -> str:
@@ -9,3 +21,33 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+#---JWT
+
+# Функция для создания JWT токена
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# Функция для проверки токена
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired"
+        )
+    except jwt.JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials"
+        )
