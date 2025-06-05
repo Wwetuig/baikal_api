bd54b007cd3b05c379946e637c7169751d0f8674
WWetuig <klinkovicvadim7@gmail.com>
2025-06-05 23:33:10 +0700

    feat: added all routers

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from api.v1 import users, files
+from api.v1 import users, satellite_data_files, ground_data_files
+from api.v1 import external_resources
 
 # Создание FastAPI приложения
 app = FastAPI()
 
 #CORS Configuring
 origins = [
-    "http://localhost:3000",
-    "127.0.0.1:3000",
+    "http://localhost:8080",
 ]
 
 app.add_middleware(
@@ -17,12 +17,21 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["Access-Control-Allow-Origin"],
 )
 
 # Подключаем роутеры
 app.include_router(users.user_router, prefix="/api/v1/users", tags=["Users"])
-app.include_router(files.files_router, prefix="/api/v1/files", tags=["Files"])
+
+app.include_router(satellete_data_files.satellite_data_firstSD_router, prefix="/api/v1/files/satellite_data", tags=["Satellite data (Landsat)"])
+app.include_router(satellete_data_files.satellite_data_secondSD_router, prefix="/api/v1/files/satellite_data", tags=["Satellite data (Monthly Avg)"])
+app.include_router(satellete_data_files.satellite_data_thirdSD_router, prefix="/api/v1/files/satellite_data", tags=["Satellite data (Monthly Avg Many Years)"])
+app.include_router(satellete_data_files.satellite_data_router, prefix="/api/v1/files/satellite_data", tags=["Satellite data (general)"])
+
+app.include_router(ground_data_files.ground_data_router, prefix="/api/v1/files/ground_data", tags=["Ground data"])
+
+app.include_router(external_resources.external_resources_router, prefix="/api/v1/external_resources", tags=["External Resources"])
+
 
 @app.get("/")
 async def read_root():
